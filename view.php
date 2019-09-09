89e826f935f527d0027b1ea4e11c89b93bd1e0c0
Richard <richard0persson@gmail.com>
2019-09-09 15:12:08 +0200

    Pull request

--- view.php
+++ view.php
@@ -1,23 +1,37 @@
 <?php
 
-require_once("functions.php");
+require_once("MetricFunctions.php");
 
-// Klass som renderar huvudinnehållet
+// Render the main content
 class View {
-    
-
     private $textInput = "";
-    private $inputReceived = false;
+    private $funcs;
 
     public function __construct(string $text) {
         $this->textInput = $text;
+        $this->funcs = new MetricFunctions($this->textInput);
     }
     
      function show() {
-        if ($this->inputReceived) {
-            $funcs = new functions($this->textInput);
-        }
-        //ska returera vy beroende på om det finns en text fil eller inte
+        if (strlen($this->textInput) > 0) {
+            return $this->showMetrics();
+        }        
     }
 
+    function showMetrics () {
+        $numberOfChars = $this->funcs->getCountedChars();
+        $numberOfForLoops = $this->funcs->getCountedForLoops();
+        $numberOfForEachLoops = $this->funcs->getCountedForEachLoops();
+        $numberOfIfStatements = $this->funcs->getCountedIfStatements();
+
+
+        return $this->metricsDataResults = "
+        <ul>
+            <li> Number of characters: $numberOfChars </li>
+            <li> Number of For-Loops: $numberOfForLoops </li>
+            <li> Number of ForEach-Loops: $numberOfForEachLoops </li>
+            <li> Number of If-statements: $numberOfIfStatements </li>
+        </ul>
+        ";
+    }
 }
